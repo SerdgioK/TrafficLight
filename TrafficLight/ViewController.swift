98814446b32c2f9b15ff298b18aa2ffabe7b76ad
SERDGIO <SERDGIOK@YANDEX.RU>
2022-09-19 18:21:48 +0300

    TestCode1

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var redSignalView: UIView!
+    @IBOutlet var yellowSignalView: UIView!
+    @IBOutlet var greenSignalView: UIView!
+    @IBOutlet var startButton: UIButton!
+    var counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redSignalView.alpha = 0.3
+        yellowSignalView.alpha = 0.3
+        greenSignalView.alpha = 0.3
     }
 
-
+    @IBAction func startButtonDidTab(_ sender: UIButton) {
+        counter += 1
+        switch counter {
+        case 1:
+            redSignalView.alpha = 1
+        case 2:
+            redSignalView.alpha = 0.3
+            yellowSignalView.alpha = 1
+        case 3:
+            yellowSignalView.alpha = 0.3
+            greenSignalView.alpha = 1
+        default:
+            break
+        }
+    }
+    
 }
 
